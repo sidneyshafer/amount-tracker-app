699dbc826458e8c0c5d56e6d5d1cb8dae2b9ba8b
Sidney Shafer <sidney@mnshafers.com>
2023-05-31 18:15:34 -0500

    changed css style and js code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,10 +7,10 @@ function ExpenseItem() {
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate}</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
         <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
