10f28414e13d5161014633257f5bc7b7c52705de
Sidney Shafer <sidney@mnshafers.com>
2023-06-05 06:22:19 -0500

    Created Expense.js and Expense.css

---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+  const expense = props.expenses;
+
+  return (
+    <div className='expenses'>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
