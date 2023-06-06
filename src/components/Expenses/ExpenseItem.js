09b58e1cba63c5e2f28c283fd37247a6c60aed8b
Sidney Shafer <sidney@mnshafers.com>
2023-06-06 06:42:15 -0500

    implemented useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // React hook - start with 'use'; must be called inside component function
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    // state updating function
+    setTitle('Updated!');
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
