a66c0f739cabaf268014c2593510106b1c231a0f
Sidney Shafer <sidney@mnshafers.com>
2023-06-07 08:41:31 -0500

    added filter functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,12 +7,15 @@ import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const expense = props.items;
   const [filteredYear, setFilteredYear] = useState('2023');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -21,26 +24,14 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={expense[0].title}
-          amount={expense[0].amount}
-          date={expense[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expense[1].title}
-          amount={expense[1].amount}
-          date={expense[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expense[2].title}
-          amount={expense[2].amount}
-          date={expense[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expense[3].title}
-          amount={expense[3].amount}
-          date={expense[3].date}
-        ></ExpenseItem>
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
