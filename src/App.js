5d8dd0f2383bad464c89a50078444d8ce1bfa9db
Sidney Shafer <sidney@mnshafers.com>
2023-06-08 05:48:29 -0500

    Changed code in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NewExpense from './components/NewExpense/NewExpense';
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
+    title: 'Paper Towels',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
